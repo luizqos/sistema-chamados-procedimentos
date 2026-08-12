8942d2f3cba2c2b82740fcd42ff70752ee0e4944
luiz <luiz.qos@gmail.com>
2026-08-12 10:53:03 -0300

    Adiconado permissão de dev origins

--- frontend-chamados/next.config.js
+++ frontend-chamados/next.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextStyle}.NextConfig */
+/** @type {import('next').NextConfig} */
+const allowedOrigins = process.env.ALLOWED_DEV_ORIGINS 
+  ? process.env.ALLOWED_DEV_ORIGINS.split(',').map(origin => origin.trim()) 
+  : [];
+
 const nextConfig = {
+  allowedDevOrigins: allowedOrigins,
+
   typescript: {
      ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+}
 
 module.exports = nextConfig;
